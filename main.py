43f90bd47553767fa706c61af1b1006927d4dbbc
Harry Rudolph <harryrudolph@me.com>
2020-08-25 21:43:38 +0100

    rewrote functions so that they check if a value is possible. Wrote a solve function that uses back tracking to solve a sudoku.

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
+clear = "\n" *100
 
-board = [[1, 2, 3, 4, 5, 6, 7, 8, 9],
-         [4, 5, 6, 5, 6, 7, 8, 9, 1],
-         [7, 8, 9, 6, 7, 8, 9, 1, 2],
-         [4, 5, 6, 7, 8, 9, 1, 2, 3],
-         [5, 6, 7, 8, 9, 1, 2, 3, 4],
-         [6, 7, 8, 9, 1, 2, 3, 4, 5],
-         [7, 8, 9, 1, 2, 3, 4, 5, 6],
-         [8, 9, 1, 2, 3, 4, 5, 6, 7],
-         [9, 1, 2, 3, 4, 5, 6, 7, 8]]
+
+boardHard = [[0, 0, 0, 0, 0, 7, 0, 0, 0],
+         [0, 0, 0, 8, 9, 0, 5, 0, 0],
+         [0, 4, 0, 0, 0, 3, 0, 0, 0],
+         [0, 0, 0, 0, 3, 0, 0, 6, 0],
+         [7, 0, 3, 2, 0, 9, 0, 0, 0],
+         [9, 0, 0, 6, 0, 1, 0, 0, 4],
+         [0, 5, 0, 0, 0, 2, 4, 0, 0],
+         [0, 0, 6, 4, 0, 0, 2, 0, 8],
+         [0, 0, 0, 0, 0, 0, 7, 0, 3]]
+
+boardEasy = [[0, 0, 3, 5, 6, 0, 4, 7, 0],
+         [0, 0, 0, 3, 4, 2, 0, 0, 1],
+         [0, 2, 0, 1, 9, 7, 0, 0, 0],
+         [4, 3, 0, 0, 0, 6, 8, 1, 0],
+         [2, 0, 6, 8, 0, 0, 0, 9, 0],
+         [9, 0, 0, 0, 0, 3, 0, 6, 0],
+         [0, 0, 0, 6, 0, 5, 1, 2, 3],
+         [0, 5, 8, 0, 0, 9, 7, 0, 0],
+         [0, 0, 2, 0, 3, 4, 5, 0, 0]]
+
+board = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
+         [6, 0, 0, 1, 9, 5, 0, 0, 0],
+         [0, 9, 8, 0, 0, 0, 0, 6, 0],
+         [8, 0, 0, 0, 6, 0, 0, 0, 3],
+         [4, 0, 0, 8, 0, 3, 0, 0, 1],
+         [7, 0, 0, 0, 2, 0, 0, 0, 6],
+         [0, 6, 0, 0, 0, 0, 2, 8, 0],
+         [0, 0, 0, 4, 1, 9, 0, 0, 5],
+         [0, 0, 0, 0, 8, 0, 0, 7, 9]]
 
 
 def renderBoard():
@@ -27,73 +49,58 @@ def renderBoard():
             print("=====================")
     
 
-def checkRow(rowNum):
-    #Loop through the row, adding each cell to a list. Before adding check to see if it is already in the list
-    compare = []
-    
+def possibleRow(rowNum, n):
     for x in range(9):
-        for i in compare:
-            if i == board[rowNum][x]:
-                return False
-            
-        compare.append(board[rowNum][x])
-    return True
+        if board[rowNum][x] == n:
+            return False
 
-def checkColumn(colNum):
-    compare = []
-    
+    return True
+        
+
+def possibleColumn(colNum, n):
     for y in range(9):
-        for i in compare:
-            if i == board[y][colNum]:
+        if board[y][colNum] == n:
+            return False
+
+    return True
+
+
+
+def possibleBox(colNum, rowNum, n):
+    boxX = (colNum // 3) * 3
+    boxY = (rowNum // 3) * 3
+    
+    
+    for y in range(boxY, boxY + 3):
+        for x in range(boxX, boxX + 3):
+            if board[y][x] == n:
                 return False
-            
-        compare.append(board[y][colNum])
-
     return True
 
 
+def possible(x, y, n):
+    if possibleColumn(x, n) and possibleRow(y, n) and possibleBox(x, y, n):
+        return True
+    return False
 
-def checkBox(boxNum):
-    #Box numbers are 1-9
-    compare = []
+def solve():
+    for y in range(9):
+        for x in range(9):
+            if board[y][x] == 0: 
+                for i in range(1, 10):
+                    if possible(x, y, i):
+                        board[y][x] = i
+                        # print("boardxy =: ", board[y][x], "| i = ", i) 
+                        # renderBoard()
+                        # print("#################################################")
+                        solve()
+                        # print("why not run again?")
+                        board[y][x] = 0
+                return
+    renderBoard()
 
-    # for box in range(9):
-    #I'm sure there is a better way. Somehow I need to set y based on boxNum
-
-    if boxNum == 1 or boxNum == 2 or boxNum == 3:
-        yArray = [0, 1, 2]
-    if boxNum == 4 or boxNum == 5 or boxNum == 6:
-        yArray = [3, 4, 5]
-    if boxNum == 7 or boxNum == 8 or boxNum == 9:
-        yArray = [6, 7, 8]
-    
-    for y in yArray:
-        for x in range(3):
-            for i in compare:
-                if i == board[y][x*(boxNum % 3)]:
-                #    print("compare: ", compare, "| x = ", x, "| y = ", y)
-
-                    print("the box is bad")
-                    return False
-            compare.append(board[y][x*boxNum% 3])
-
-    print("the box is fine")
-    return True
-
-
+print(clear)
 renderBoard()
-
-if checkColumn(0):
-    print("the column is fine")
-else:
-    print("the column is not fine")
-
-    
-if checkRow(0):
-    print("the line is fine")
-else:
-    print("the line is not fine")
-
-
-
-checkBox(1)
+print("Now solved:")
+solve()
+# renderBoard()
