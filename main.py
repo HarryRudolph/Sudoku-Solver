38ba333eeec33d71ebfbdb888060b3a62971f0d7
Harry Rudolph <harryrudolph@me.com>
2020-08-31 22:36:57 +0100

    pressing 1 now solves the board

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption(title)
 
 font = pygame.font.SysFont("comicsans", 50)
+clock = pygame.time.Clock()
 
 #x0, y0 is top left
     
@@ -28,7 +29,7 @@ yScale = 50
 
 
 
-board = [[0, 0, 0, 0, 0, 7, 0, 0, 0],
+boardHard = [[0, 0, 0, 0, 0, 7, 0, 0, 0],
          [0, 0, 0, 8, 9, 0, 5, 0, 0],
          [0, 4, 0, 0, 0, 3, 0, 0, 0],
          [0, 0, 0, 0, 3, 0, 0, 6, 0],
@@ -38,7 +39,7 @@ board = [[0, 0, 0, 0, 0, 7, 0, 0, 0],
          [0, 0, 6, 4, 0, 0, 2, 0, 8],
          [0, 0, 0, 0, 0, 0, 7, 0, 3]]
 
-boardEasy = [[0, 0, 3, 5, 6, 0, 4, 7, 0],
+board = [[0, 0, 3, 5, 6, 0, 4, 7, 0],
          [0, 0, 0, 3, 4, 2, 0, 0, 1],
          [0, 2, 0, 1, 9, 7, 0, 0, 0],
          [4, 3, 0, 0, 0, 6, 8, 1, 0],
@@ -101,17 +102,34 @@ def possible(x, y, n):
         return True
     return False
 
-def solve():
+
+def findEmpty():
     for y in range(9):
         for x in range(9):
-            if board[y][x] == 0: 
-                for i in range(1, 10):
-                    if possible(x, y, i):
-                        board[y][x] = i
-                        solve()
-                        board[y][x] = 0
-                return
+            if board[y][x] == 0:
+                return (y, x)
+    return False
+
+def solve():
+    empty = findEmpty() 
+
+    if not empty:
+        return True
+    else:
+        y, x = empty
+        
+        
+    for i in range(1, 10):
+        if possible(x, y, i):
+            board[y][x] = i
+            if solve():
+                return True
+                        
+            board[y][x] = 0
+
+    return False
     renderConsoleBoard()
+    
 
 
 
@@ -144,27 +162,29 @@ def renderGrid():
 
 
 def renderBoard():
-    for x in range(9):
-        for y in range(9):
+    for y in range(9):
+        for x in range(9):
             temp = 0
-            temp = temp + board[x][y]
+            temp = temp + board[y][x]
             text = font.render(str(temp), 0, (0,0,0))
             screen.blit(text, ((gridx0 + 15)+ (x * xScale), (gridy0 + 10) + (y *yScale)))
-    
-
-
-    
+        
 
 while running:
     screen.fill((220, 221, 225))    
     renderGrid()
     renderBoard()
-    pygame.display.update()
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_1:
+                solve()
+            if event.key == pygame.K_2:
+                #board[0][0]= 9
+                renderConsoleBoard()
 
-
-#pygame.display.flip()            
-
+    pygame.display.update()
+    clock.tick(1)
+    
